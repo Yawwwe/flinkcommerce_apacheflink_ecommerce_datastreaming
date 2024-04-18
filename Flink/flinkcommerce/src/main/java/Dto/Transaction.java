d6672921bb76c8c1e85f3b260e50e76cd420cb16
Yawe <gonzalo.12.98@gmail.com>
2024-04-18 22:37:46 +0200

    First commit

---
+++ Flink/flinkcommerce/src/main/java/Dto/Transaction.java
@@ -0,0 +1,33 @@
+package Dto;
+import java.sql.Timestamp;
+import lombok.Data;
+
+@Data
+public class Transaction {
+    
+    private String transactionId;
+
+    private String productId;
+
+    private String productName;
+
+    private String productCategory;
+
+    private double productPrice;
+
+    private int productQuantity;
+
+    private String productBrand;
+
+    private String currency;
+
+    private String customerId;
+
+    private double totalAmount;
+
+    private Timestamp transactionDate;
+
+    private String paymentMethod;
+
+
+}
---
+++ Flink/flinkcommerce/src/main/java/utils/JsonUtil.java
@@ -0,0 +1,22 @@
+package utils;
+
+import Dto.Transaction;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class JsonUtil {
+    
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String convertTransactionToJson(Transaction transaction) {
+
+    try{
+        return objectMapper.writeValueAsString(transaction);
+    } catch (JsonProcessingException e) {
+        e.printStackTrace();
+        return null;
+    }
+
+
+}
+}
